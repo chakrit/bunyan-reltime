a15582386ca96f44db0a383f71e35df0970bac3e
Chakrit Wichian <chakrit@oozou.com>
2013-10-24 18:13:18 +0700

    Initial implementation.

---
+++ index.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+
+// index.js - Main export.
+module.exports = (function(undefined) {
+
+  var asesrt = require('assert')
+    , reltime = require('reltime')
+    , JSONStream = require('JSONStream')
+    , optimist = require('optimist')
+    , moment = require('moment')
+    , stream = require('stream')
+    , util = require('util');
+
+  function TimeFilterStream(reference) {
+    stream.Transform.call(this, { objectMode: true });
+    this.reference = reference;
+  }
+
+  util.inherits(TimeFilterStream, stream.Transform);
+
+  TimeFilterStream.prototype._transform = function(chunk, encoding, callback) {
+    var time = moment(chunk.time);
+    if (time.isAfter(this.reference))
+      this.push(chunk);
+
+    return callback(null);
+  };
+
+  return function filter(reference, relative) {
+    if (typeof relative === 'undefined') {
+      relative = reference;
+      reference = undefined;
+    }
+
+    reference = reltime.parse(reference || new Date(), relative);
+    reference = moment(reference);
+
+    process.stdin
+      .pipe(JSONStream.parse())
+      .pipe(new TimeFilterStream(reference))
+      .pipe(JSONStream.stringify())
+      .pipe(process.stdout);
+  };
+})();
+
+if (require.main === module) {
+  var argv = Array.prototype.slice.call(process.argv);
+  argv.shift(); // node
+  argv.shift(); // index.js
+
+  if (argv.length === 0) {
+    console.error("bunyan-reltime [reference-time] relative-time");
+    return process.exit(1);
+  }
+
+  module.exports.apply(this, argv);
+}
+
